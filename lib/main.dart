4603f4c833124f2a6cc1212a18d5960c3b817785
wilson <yxw_android@163.com>
2019-06-14 19:08:43 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,79 +10,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: VoiceView(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class VoiceView extends StatefulWidget {
+  VoiceView({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _VoiceViewState createState() => _VoiceViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _VoiceViewState extends State<VoiceView> {
+  double start = 0.0;
+  double offset = 0.0;
+  bool isUp = false;
+  String textShow = "按住说话";
+  String toastShow = "手指上滑,取消发送";
 
-
-
-  @override
-  void initState() {
-    super.initState();
-
-
-    String ss ='[转圈][转圈][转圈][转圈]';
-    var  listSs= getContents(ss);
-
-
-  }
-
-
-  ///获取文本数组
-  static List<String> getContents(String content){
-    var emojiReg =   new RegExp("(\\[.+?\\])");
-    var emojiMatches = emojiReg.allMatches(content);
-
-    List<String>  emojis = new List();
-    for (Match m in emojiMatches) {
-      String match = m.group(0);
-      emojis.add(match);
-    }
-
-    String itemStr=content;
-    for(String item in emojis){
-      itemStr = itemStr.replaceAll(item, "&");
-    }
-
-    var  itemSps= itemStr.split("&");
-    List<String> tempArr=new List();
-    for(String item in itemSps){
-      if(item!=null&&item!=""){
-        tempArr.add(item);
-      }
-    }
-    tempArr.addAll(emojis);
-
-    Map<int,String> mapStr = new Map();
-    for(int i = 0;i<tempArr.length;i++){
-      String  item = tempArr[i];
-      int  itemLeng  = item.length;
-      int itemPoision = content.indexOf(item);
-      String itemStr =content.substring(itemPoision,itemPoision+itemLeng);
-      mapStr[itemPoision] = itemStr;
-    }
-
-    var sortedKeys = mapStr.keys.toList()..sort();
-    List<String> contentArr=new List();
-    for(int item in sortedKeys){
-      contentArr.add( mapStr[item]);
-    }
-
-    return contentArr;
-
-  }
+  bool isVoiceState = true; ///默认隐藏状态
 
   @override
   Widget build(BuildContext context) {
@@ -90,14 +39,80 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button:  en ',
+      body: GestureDetector(
+        onVerticalDragStart: (details) {
+          start = details.globalPosition.dy;
+          setState(() {
+            textShow = "松开结束";
+          });
+        },
+        onVerticalDragEnd: (details) {
+          setState(() {
+            textShow = "按住说话";
+            isVoiceState = true;
+          });
+        },
+        onVerticalDragUpdate: (details) {
+          offset = details.globalPosition.dy;
+
+          print(offset - start);
+          setState(() {
+            isUp = start - offset > 100 ? true : false;
+            if (isUp) {
+              textShow = "松开手指,取消发送";
+              toastShow= textShow;
+              isVoiceState = false;
+            } else {
+              textShow = "松开结束";
+              toastShow = "手指上滑,取消发送";
+            }
+          });
+        },
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Colors.white,
+          child: Offstage(
+            offstage: isVoiceState,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Opacity(
+                    opacity: 0.5,
+                    child: Container(
+                      width: 160,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Color(0xff77797A),
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin:EdgeInsets.only(top: 10),
+                            child: new Image.asset(
+                              "images/voice_volume_7.png",
+                              width: 100,
+                              height: 100,
+                            ),
+                          ),
+                          Container(
+                            padding:EdgeInsets.only(right:20,left:20,top: 0),
+                            child: Text(
+                              toastShow,style: TextStyle(color: Colors.white),),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  textShow,
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );

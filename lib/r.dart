b029c62f2abd8da05768d4551e119673662adaa2
wilson <yxw_android@163.com>
2019-06-22 18:29:18 +0800

    update

---
+++ lib/r.dart
@@ -0,0 +1,214 @@
+class R {
+  /// ![](http://127.0.0.1:9528/images/transfer_ico.png)
+  static final String imagesTransferIco = 'images/transfer_ico.png';
+  /// ![](http://127.0.0.1:9528/images/photo_ico.png)
+  static final String imagesPhotoIco = 'images/photo_ico.png';
+  /// ![](http://127.0.0.1:9528/images/material_ico.png)
+  static final String imagesMaterialIco = 'images/material_ico.png';
+  /// ![](http://127.0.0.1:9528/images/voice_left.png)
+  static final String imagesVoiceLeft = 'images/voice_left.png';
+  /// ![](http://127.0.0.1:9528/images/file_ico.png)
+  static final String imagesFileIco = 'images/file_ico.png';
+  /// ![](http://127.0.0.1:9528/images/img.jpg)
+  static final String imagesImg = 'images/img.jpg';
+  /// ![](http://127.0.0.1:9528/images/camera_ico.png)
+  static final String imagesCameraIco = 'images/camera_ico.png';
+  /// ![](http://127.0.0.1:9528/images/chat_room_left_voice.png)
+  static final String imagesChatRoomLeftVoice = 'images/chat_room_left_voice.png';
+  /// ![](http://127.0.0.1:9528/images/left_voice_3.png)
+  static final String imagesLeftVoice3 = 'images/left_voice_3.png';
+  /// ![](http://127.0.0.1:9528/images/pwd.png)
+  static final String imagesPwd = 'images/pwd.png';
+  /// ![](http://127.0.0.1:9528/images/left_voice_2.png)
+  static final String imagesLeftVoice2 = 'images/left_voice_2.png';
+  /// ![](http://127.0.0.1:9528/images/voice_volume_1.png)
+  static final String imagesVoiceVolume1 = 'images/voice_volume_1.png';
+  /// ![](http://127.0.0.1:9528/images/voice_volume_3.png)
+  static final String imagesVoiceVolume3 = 'images/voice_volume_3.png';
+  /// ![](http://127.0.0.1:9528/images/left_voice_1.png)
+  static final String imagesLeftVoice1 = 'images/left_voice_1.png';
+  /// ![](http://127.0.0.1:9528/images/voice_volume_2.png)
+  static final String imagesVoiceVolume2 = 'images/voice_volume_2.png';
+  /// ![](http://127.0.0.1:9528/images/voice_volume_6.png)
+  static final String imagesVoiceVolume6 = 'images/voice_volume_6.png';
+  /// ![](http://127.0.0.1:9528/images/cancel_voice.png)
+  static final String imagesCancelVoice = 'images/cancel_voice.png';
+  /// ![](http://127.0.0.1:9528/images/voice_too_short_ico.png)
+  static final String imagesVoiceTooShortIco = 'images/voice_too_short_ico.png';
+  /// ![](http://127.0.0.1:9528/images/voice_volume_7.png)
+  static final String imagesVoiceVolume7 = 'images/voice_volume_7.png';
+  /// ![](http://127.0.0.1:9528/images/voice_volume_5.png)
+  static final String imagesVoiceVolume5 = 'images/voice_volume_5.png';
+  /// ![](http://127.0.0.1:9528/images/voice_volume_4.png)
+  static final String imagesVoiceVolume4 = 'images/voice_volume_4.png';
+  /// ![](http://127.0.0.1:9528/images/delete.png)
+  static final String imagesDelete = 'images/delete.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck27.png)
+  static final String imagesEmojiFuck27 = 'images/emoji/fuck27.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck33.png)
+  static final String imagesEmojiFuck33 = 'images/emoji/fuck33.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck32.png)
+  static final String imagesEmojiFuck32 = 'images/emoji/fuck32.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck26.png)
+  static final String imagesEmojiFuck26 = 'images/emoji/fuck26.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck30.png)
+  static final String imagesEmojiFuck30 = 'images/emoji/fuck30.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck24.png)
+  static final String imagesEmojiFuck24 = 'images/emoji/fuck24.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck18.png)
+  static final String imagesEmojiFuck18 = 'images/emoji/fuck18.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck19.png)
+  static final String imagesEmojiFuck19 = 'images/emoji/fuck19.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck31.png)
+  static final String imagesEmojiFuck31 = 'images/emoji/fuck31.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck09.png)
+  static final String imagesEmojiFuck09 = 'images/emoji/fuck09.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck35.png)
+  static final String imagesEmojiFuck35 = 'images/emoji/fuck35.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck21.png)
+  static final String imagesEmojiFuck21 = 'images/emoji/fuck21.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck20.png)
+  static final String imagesEmojiFuck20 = 'images/emoji/fuck20.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck34.png)
+  static final String imagesEmojiFuck34 = 'images/emoji/fuck34.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck08.png)
+  static final String imagesEmojiFuck08 = 'images/emoji/fuck08.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck22.png)
+  static final String imagesEmojiFuck22 = 'images/emoji/fuck22.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck37.png)
+  static final String imagesEmojiFuck37 = 'images/emoji/fuck37.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck23.png)
+  static final String imagesEmojiFuck23 = 'images/emoji/fuck23.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck93.png)
+  static final String imagesEmojiFuck93 = 'images/emoji/fuck93.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck50.png)
+  static final String imagesEmojiFuck50 = 'images/emoji/fuck50.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck79.png)
+  static final String imagesEmojiFuck79 = 'images/emoji/fuck79.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck51.png)
+  static final String imagesEmojiFuck51 = 'images/emoji/fuck51.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck45.png)
+  static final String imagesEmojiFuck45 = 'images/emoji/fuck45.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck86.png)
+  static final String imagesEmojiFuck86 = 'images/emoji/fuck86.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck90.png)
+  static final String imagesEmojiFuck90 = 'images/emoji/fuck90.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck84.png)
+  static final String imagesEmojiFuck84 = 'images/emoji/fuck84.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck53.png)
+  static final String imagesEmojiFuck53 = 'images/emoji/fuck53.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck47.png)
+  static final String imagesEmojiFuck47 = 'images/emoji/fuck47.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck46.png)
+  static final String imagesEmojiFuck46 = 'images/emoji/fuck46.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck52.png)
+  static final String imagesEmojiFuck52 = 'images/emoji/fuck52.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck85.png)
+  static final String imagesEmojiFuck85 = 'images/emoji/fuck85.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck95.png)
+  static final String imagesEmojiFuck95 = 'images/emoji/fuck95.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck81.png)
+  static final String imagesEmojiFuck81 = 'images/emoji/fuck81.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck56.png)
+  static final String imagesEmojiFuck56 = 'images/emoji/fuck56.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck42.png)
+  static final String imagesEmojiFuck42 = 'images/emoji/fuck42.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck43.png)
+  static final String imagesEmojiFuck43 = 'images/emoji/fuck43.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck57.png)
+  static final String imagesEmojiFuck57 = 'images/emoji/fuck57.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck80.png)
+  static final String imagesEmojiFuck80 = 'images/emoji/fuck80.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck94.png)
+  static final String imagesEmojiFuck94 = 'images/emoji/fuck94.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck100.png)
+  static final String imagesEmojiFuck100 = 'images/emoji/fuck100.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck82.png)
+  static final String imagesEmojiFuck82 = 'images/emoji/fuck82.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck96.png)
+  static final String imagesEmojiFuck96 = 'images/emoji/fuck96.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck69.png)
+  static final String imagesEmojiFuck69 = 'images/emoji/fuck69.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck41.png)
+  static final String imagesEmojiFuck41 = 'images/emoji/fuck41.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck55.png)
+  static final String imagesEmojiFuck55 = 'images/emoji/fuck55.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck40.png)
+  static final String imagesEmojiFuck40 = 'images/emoji/fuck40.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck68.png)
+  static final String imagesEmojiFuck68 = 'images/emoji/fuck68.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck97.png)
+  static final String imagesEmojiFuck97 = 'images/emoji/fuck97.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck83.png)
+  static final String imagesEmojiFuck83 = 'images/emoji/fuck83.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck65.png)
+  static final String imagesEmojiFuck65 = 'images/emoji/fuck65.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck71.png)
+  static final String imagesEmojiFuck71 = 'images/emoji/fuck71.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck58.png)
+  static final String imagesEmojiFuck58 = 'images/emoji/fuck58.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck64.png)
+  static final String imagesEmojiFuck64 = 'images/emoji/fuck64.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck99.png)
+  static final String imagesEmojiFuck99 = 'images/emoji/fuck99.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck66.png)
+  static final String imagesEmojiFuck66 = 'images/emoji/fuck66.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck67.png)
+  static final String imagesEmojiFuck67 = 'images/emoji/fuck67.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck98.png)
+  static final String imagesEmojiFuck98 = 'images/emoji/fuck98.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck77.png)
+  static final String imagesEmojiFuck77 = 'images/emoji/fuck77.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck63.png)
+  static final String imagesEmojiFuck63 = 'images/emoji/fuck63.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck76.png)
+  static final String imagesEmojiFuck76 = 'images/emoji/fuck76.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck48.png)
+  static final String imagesEmojiFuck48 = 'images/emoji/fuck48.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/defaultEmoji.png)
+  static final String imagesEmojiDefaultEmoji = 'images/emoji/defaultEmoji.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck75.png)
+  static final String imagesEmojiFuck75 = 'images/emoji/fuck75.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck61.png)
+  static final String imagesEmojiFuck61 = 'images/emoji/fuck61.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck49.png)
+  static final String imagesEmojiFuck49 = 'images/emoji/fuck49.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck06.png)
+  static final String imagesEmojiFuck06 = 'images/emoji/fuck06.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck12.png)
+  static final String imagesEmojiFuck12 = 'images/emoji/fuck12.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/collection.png)
+  static final String imagesEmojiCollection = 'images/emoji/collection.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck13.png)
+  static final String imagesEmojiFuck13 = 'images/emoji/fuck13.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck07.png)
+  static final String imagesEmojiFuck07 = 'images/emoji/fuck07.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck11.png)
+  static final String imagesEmojiFuck11 = 'images/emoji/fuck11.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck05.png)
+  static final String imagesEmojiFuck05 = 'images/emoji/fuck05.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck39.png)
+  static final String imagesEmojiFuck39 = 'images/emoji/fuck39.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck38.png)
+  static final String imagesEmojiFuck38 = 'images/emoji/fuck38.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck04.png)
+  static final String imagesEmojiFuck04 = 'images/emoji/fuck04.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck10.png)
+  static final String imagesEmojiFuck10 = 'images/emoji/fuck10.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck28.png)
+  static final String imagesEmojiFuck28 = 'images/emoji/fuck28.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck14.png)
+  static final String imagesEmojiFuck14 = 'images/emoji/fuck14.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck01.png)
+  static final String imagesEmojiFuck01 = 'images/emoji/fuck01.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck15.png)
+  static final String imagesEmojiFuck15 = 'images/emoji/fuck15.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck29.png)
+  static final String imagesEmojiFuck29 = 'images/emoji/fuck29.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck03.png)
+  static final String imagesEmojiFuck03 = 'images/emoji/fuck03.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck16.png)
+  static final String imagesEmojiFuck16 = 'images/emoji/fuck16.png';
+  /// ![](http://127.0.0.1:9528/images/emoji/fuck02.png)
+  static final String imagesEmojiFuck02 = 'images/emoji/fuck02.png';
+}
